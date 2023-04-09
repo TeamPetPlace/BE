ebdd0a333f6e2dccfeebf03ae1e41f4d5ff15f28
Kuno1734 <dlsgus59@gmail.com>
2023-04-09 14:27:18 +0900

    fix : Mypage에서 사업자가 작성한 게시글이 없을 때 백지화되는 현상 수정

--- src/main/java/com/sparta/petplace/mypage/service/MypageService.java
+++ src/main/java/com/sparta/petplace/mypage/service/MypageService.java
@@ -52,7 +52,8 @@ public class MypageService {
         Pageable pageable = PageRequest.of(page, size);
         List<Post> found = postRepository.findAllByEmail(member.getEmail());
         if (found.isEmpty()) {
-            throw new CustomException(Error.NOT_FOUND_POST);
+            return null;
+//            throw new CustomException(Error.NOT_FOUND_POST);
         }
         List<PostResponseDto> responseDtos = new ArrayList<>();
         for (Post post : found) {
